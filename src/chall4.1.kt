50b43457d3b2b04d863c57546bf037efe2625587
lool-aladimi <loolaladimi@gmail.com>
2020-09-06 02:14:21 +0300

    Initial commit

---
+++ src/chall4.1.kt
@@ -0,0 +1,6 @@
+fun main(){
+    var lollist = listOf("NW","it","IS","MIS")
+    for ((index,value) in lollist.withIndex()){
+        println("in the index :"+ "$index"+"  "+"the department is"+"  " +"$value")
+    }
+}
